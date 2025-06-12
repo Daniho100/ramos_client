83a97d3253863fbb40d63ec7b08b6d88a888b641
Daniho100 <daniandre.dah@gmail.com>
2025-06-12 10:17:54 +0100

    hosting frontend final push

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ramos-server.vercel.app',
+  baseURL: 'https://ramos-server.vercel.app/api',
   withCredentials: true, 
 });
 
